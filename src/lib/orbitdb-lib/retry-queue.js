237059fedd8c37ebb668c8d4c25bf1a033f554ce
Chris Troutner <chris.troutner@gmail.com>
2021-06-19 11:45:09 -0700

    Moving retry queue to its own library

--- src/lib/orbitdb-lib/retry-queue.js
+++ src/lib/orbitdb-lib/retry-queue.js
@@ -5,3 +5,71 @@
   A promise-based queue allows this to happen while respecting rate-limits
   of the blockchain service provider.
 */
+
+const PQueue = require('p-queue').default
+const pRetry = require('p-retry')
+
+let _this
+
+class RetryQueue {
+  constructor (localConfig) {
+    // Encapsulate dependencies
+    this.validationQueue = new PQueue({ concurrency: 1 })
+
+    _this = this
+  }
+
+  // Add an async function to the queue, and execute it with the input object.
+  async addToQueue (funcHandle, inputObj) {
+    try {
+      const returnVal = await _this.validationQueue.add(() =>
+        _this.retryWrapper(funcHandle, inputObj)
+      )
+
+      return returnVal
+    } catch (err) {
+      console.error('Error in addToQueue()')
+      throw err
+    }
+  }
+
+  // Wrap the p-retry library.
+  // This function returns a promise that will resolve to the output of the
+  // function 'funcHandle'.
+  async retryWrapper (funcHandle, inputObj) {
+    try {
+      console.log('Entering retryWrapper()')
+
+      return pRetry(
+        async () => {
+          return await funcHandle(inputObj)
+        },
+        {
+          onFailedAttempt: _this.handleValidationError,
+          retries: 5 // Retry 5 times
+        }
+      )
+    } catch (err) {
+      console.error('Error in retryWrapper: ', err)
+      // throw err?
+    }
+  }
+
+  // Notifies the user that an error occured and that a retry will be attempted.
+  // It tracks the number of retries until it fails.
+  async handleValidationError (error) {
+    try {
+      const errorMsg = `Attempt ${error.attemptNumber} to validate entry. There are ${error.retriesLeft} retries left. Waiting before trying again.`
+      console.log(errorMsg)
+
+      const SLEEP_TIME = 30000
+      console.log(`Waiting ${SLEEP_TIME} milliseconds before trying again.`)
+      await _this.bchjs.Util.sleep(SLEEP_TIME) // 30 sec
+    } catch (err) {
+      console.error('Error in handleValidationError(): ', err)
+      throw err
+    }
+  }
+}
+
+module.exports = RetryQueue
