7a5ac2bb42c3f16202835e4c04fafc406d08f968
Daniel Gonzalez <danielhumgon@gmail.com>
2021-07-20 12:20:47 -0400

    refactor(p2wdb): Refactored OrbitDB Adapter

--- test/unit/adapters/a33-orbit.unit.js
+++ test/unit/adapters/a33-orbit.unit.js
@@ -8,13 +8,13 @@ const assert = require('chai').assert
 const OrbitDBAdapter = require('../../../src/adapters/orbit')
 const KeyValueMock = require('../mocks/model-mock.js')
 const OrbitDBMock = require('../mocks/orbitdb-mock')
-
+const config = require('../../../config')
 let uut
 let sandbox
 
-describe('#PayToWrite', () => {
+describe('#OrbitDBAdapter', () => {
   beforeEach(() => {
-    uut = new OrbitDBAdapter({ ipfs: {} })
+    uut = new OrbitDBAdapter({ ipfs: { id: () => { return 'ipfs id' } } })
 
     // Mock database dependencies.
     uut.db = new OrbitDBMock()
@@ -26,16 +26,94 @@ describe('#PayToWrite', () => {
   afterEach(() => sandbox.restore())
 
   describe('#constructor', () => {
-    // TODO
     it('should throw an error if instance of IPFS is not provided', () => {
-      assert.equal(1, 1)
+      try {
+        const _uut = new OrbitDBAdapter()
+        console.log(_uut)
+        assert.fail('unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'Must pass an instance of ipfs')
+      }
     })
   })
+  describe('#start', () => {
+    it('should start', async () => {
+      // mock function for keyvalue instance in orbitdb
+      const keyValueKakeFn = () => {
+        return new OrbitDBMock()
+      }
 
-  describe('#createDb', () => {
+      // mock for orbitdb instance
+      sandbox
+        .stub(uut.OrbitDB, 'createInstance')
+        .resolves({ keyvalue: keyValueKakeFn })
+
+      await uut.start()
+      assert.isTrue(uut.isReady)
+    })
+
     // TODO
+    it('should catch and throw errors', async () => {
+      try {
+        // Force Error
+        sandbox
+          .stub(uut, 'createDb')
+          .throws(new Error('test error'))
+
+        await uut.start()
+
+        assert.fail('unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'test error')
+      }
+    })
+  })
+  describe('#createDb', () => {
     it('should use default db name in config file if name is not provided', async () => {
-      assert.equal(1, 1)
+      // mock function for keyvalue instance in orbitdb
+      const keyValueKakeFn = (dbName) => {
+        assert.equal(dbName, config.orbitDbName, 'expected to use default db name')
+        return new OrbitDBMock()
+      }
+
+      // mock for orbitdb instance
+      sandbox
+        .stub(uut.OrbitDB, 'createInstance')
+        .resolves({ keyvalue: keyValueKakeFn })
+
+      await uut.createDb()
+      assert.isTrue(uut.isReady)
+    })
+
+    it('should use db name provided', async () => {
+      const myDbName = 'myDbName'
+
+      // mock function for keyvalue instance in orbitdb
+      const keyValueFakeFn = (dbName) => {
+        assert.equal(dbName, myDbName, 'expected to use db name provided')
+        return new OrbitDBMock()
+      }
+
+      // mock for orbitdb instance
+      sandbox
+        .stub(uut.OrbitDB, 'createInstance')
+        .resolves({ keyvalue: keyValueFakeFn })
+
+      await uut.createDb(myDbName)
+      assert.isTrue(uut.isReady)
+    })
+    it('should catch and throw errors', async () => {
+      try {
+        // Force Error
+        sandbox
+          .stub(uut.OrbitDB, 'createInstance')
+          .throws(new Error('test error'))
+
+        await uut.createDb()
+        assert.fail('unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'test error')
+      }
     })
   })
 
@@ -45,10 +123,14 @@ describe('#PayToWrite', () => {
 
       assert.isObject(result)
     })
-
-    // TODO
     it('should catch and throw errors', () => {
-      assert.equal(1, 1)
+      try {
+        uut.db = undefined
+        uut.readAll()
+        assert.fail('unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'Cannot read property')
+      }
     })
   })
 })
